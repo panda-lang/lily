2aa0780039cb6d177a0325611d87853988ab5542
dzikoysk <dzikoysk@dzikoysk.net>
2016-11-05 16:12:45 +0100

    Fixed automatic registration by annotations

--- src/main/java/org/panda_lang/lily/plugin/PluginProperties.java
+++ src/main/java/org/panda_lang/lily/plugin/PluginProperties.java
@@ -1,5 +1,13 @@
 package org.panda_lang.lily.plugin;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+
 public @interface PluginProperties {
 
     String name();
