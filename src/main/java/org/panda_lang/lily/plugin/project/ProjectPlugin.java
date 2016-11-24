822a48e54c36dbb85a23896281d618943058dbc2
dzikoysk <dzikoysk@dzikoysk.net>
2016-11-24 19:42:53 +0100

    Added ProjectView plugin and updated dependencies.

--- src/main/java/org/panda_lang/lily/plugin/project/ProjectPlugin.java
+++ src/main/java/org/panda_lang/lily/plugin/project/ProjectPlugin.java
@@ -1,9 +1,14 @@
 package org.panda_lang.lily.plugin.project;
 
+import javafx.scene.layout.BorderPane;
 import org.panda_lang.lily.Lily;
 import org.panda_lang.lily.LilyConstants;
 import org.panda_lang.lily.plugin.LilyPlugin;
 import org.panda_lang.lily.plugin.PluginProperties;
+import org.panda_lang.lily.ui.LilyLayout;
+import org.panda_lang.lily.ui.LilyUI;
+
+import java.io.File;
 
 @PluginProperties(name = "Project", version = LilyConstants.VERSION)
 public class ProjectPlugin extends LilyPlugin {
@@ -13,6 +18,20 @@ public class ProjectPlugin extends LilyPlugin {
     @Override
     public void onEnable(Lily lily) {
         this.projectView = new ProjectView(this);
+
+        LilyUI ui = lily.getUI();
+        LilyLayout layout = ui.getLayout();
+        BorderPane pane = layout.getBorderPane();
+
+        LilyLayout projectLayout = new LilyLayout("project");
+        layout.addLayout(projectLayout);
+
+        BorderPane projectBorderPane = projectLayout.getBorderPane();
+        projectBorderPane.setLeft(projectView);
+        pane.setCenter(projectBorderPane);
+
+        File currentDirectory = new File(".");
+        projectView.open(currentDirectory);
     }
 
     @Override
